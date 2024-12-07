c647e1be59a4fa488a4db1fc4b4df202a08ad004
chemutai8 <dianachemutai2030@gmail.com>
2024-12-07 09:13:36 +0300

    ngrok set up

--- ticketing_system/settings.py
+++ ticketing_system/settings.py
@@ -10,7 +10,22 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# settings.py
+
+# settings.py
+
+# settings.py
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'abd1-154-159-252-162.ngrok-free.app',  # Add your new ngrok URL here
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://abd1-154-159-252-162.ngrok-free.app',  # Add your new ngrok URL here
+]
+
 
 # Application definition
 INSTALLED_APPS = [
@@ -24,6 +39,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'events.apps.EventsConfig',
     'users.apps.UsersConfig',
+    'django_daraja',  # Add this line for Mpesa integration
 ]
 
 MIDDLEWARE = [
@@ -95,3 +111,56 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'event-list'
 LOGIN_URL = 'login'
+
+# Mpesa configuration
+MPESA_ENVIRONMENT = 'sandbox'
+
+# Credentials for the daraja app
+MPESA_CONSUMER_KEY = 'E8TuRgKfVjejENvvcNGVqBmo0k39jPy1AxGGzR59wyxxsQgB'
+MPESA_CONSUMER_SECRET = 'upvPl7va5GCKsv07HJGpWQOlAnX33ux7shM2awNWYkAOHIBRANxdth4IBhsiUEPz'
+
+# Shortcode to use for transactions. For sandbox use the Shortcode 1 provided on test credentials page
+MPESA_SHORTCODE = '174379'
+
+# Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
+MPESA_EXPRESS_SHORTCODE = '174379'
+
+# Type of shortcode
+# Possible values:
+# - paybill (For Paybill)
+# - till_number (For Buy Goods Till Number)
+MPESA_SHORTCODE_TYPE = 'paybill'
+
+# Lipa na MPESA Online passkey
+MPESA_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+
+# Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
+MPESA_INITIATOR_USERNAME = 'testapi'
+
+# Plaintext password for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
+MPESA_INITIATOR_SECURITY_CREDENTIAL = 'Safaricom999!*!'
+
+# settings.py
+
+# settings.py
+
+# settings.py
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
